ffd1f3f25f6467c0184b2ba0f0de7d01b7ac5b14
MichalAdorno <>
2019-09-19 19:10:37 +0200

    init

---
+++ src/main/java/com/demo/mykeycloak/MykeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mykeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykeycloakApplication.class, args);
+	}
+
+}
